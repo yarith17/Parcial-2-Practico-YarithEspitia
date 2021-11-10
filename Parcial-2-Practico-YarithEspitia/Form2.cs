fbe35599a0323292714d5ca3fb59f70e75c6bbb6
Yarith Fernanda Espitia Ortiz <yarith.espitia.2021@upb.edu.co>
2021-11-09 19:37:14 -0500

    commit

--- Parcial-2-Practico-YarithEspitia/Form2.cs
+++ Parcial-2-Practico-YarithEspitia/Form2.cs
@@ -34,7 +34,7 @@ namespace Parcial_2_Practico_YarithEspitia
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e
         {
             Conductor driver = new Conductor();
             driver.Nombre = textBox2.Text;
